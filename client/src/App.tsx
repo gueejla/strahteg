a1f00b71850dbef1a68e993cca5fdd2ffc3c2ea6
gueejla <gueejla@gmail.com>
2025-06-16 11:09:12 -0500

    basic cursor testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [timestamp, setTimestamp] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimestamp(new Date())
+    }, 1000)
+
+    return () => clearInterval(timer)
+  }, [])
 
   return (
     <>
-      <h2>stratego implementation coming soon!</h2>
+      <h2>strahteg implementation coming soon!</h2>
+      <p>Current time: {timestamp.toLocaleTimeString()}</p>
     </>
   )
 }
